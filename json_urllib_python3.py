14fd41d078e200d63f120e58585c988778a9a166
jogeo555 <forjoseph@gmail.com>
2017-08-08 10:31:24 -0700

    first copy

---
+++ json_urllib_python3.py
@@ -0,0 +1,63 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import json
+
+
+# In[6]:
+
+
+import urllib.request
+from urllib.parse import urlencode
+
+
+# In[3]:
+
+
+addr = input("Enter address:")
+
+
+# In[4]:
+
+
+url_prefix = "https://maps.googleapis.com/maps/api/geocode/json?"
+
+
+# In[7]:
+
+
+url = url_prefix + urllib.parse.urlencode({"sensor":'false',"address":addr })
+
+
+# In[8]:
+
+
+print(url)
+
+
+# In[9]:
+
+
+data = json.loads(urllib.request.urlopen(url).read())
+
+
+# In[12]:
+
+
+print (data["results"][0]["geometry"]["location"]["lat"])
+
+
+# In[14]:
+
+
+print (data["results"][0]["formatted_address"])
+
+
+# In[ ]:
+
+
+
+
